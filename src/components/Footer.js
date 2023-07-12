618f7cc42b8b299870757dc85af9cd6186a33ac4
dbrewertx <50505000+dbrewertx@users.noreply.github.com>
2023-07-11 20:25:59 -0500

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,6 @@
 export default function Footer() {
   return (
     <>
-      <p>&copy;2023 Creative Origins Web Dev</p>
-      <p><a href="https://www.thesaintmaker.com" alt="www.thesaintmaker.com">www.thesaintmaker.com</a></p>
+      <p>&copy;2023 Doug Brewer Web Dev</p>
     </>
   );
-}
