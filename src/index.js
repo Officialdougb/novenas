2618ecab9fd7897f076132be0c4a0b90845729d2
dbrewertx <50505000+dbrewertx@users.noreply.github.com>
2023-07-11 20:51:34 -0500

    restructured app entry point app.js and index.js to make more sense

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HomeView from './components/HomeView';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HomeView />
+    <App />
   </React.StrictMode>
 );
