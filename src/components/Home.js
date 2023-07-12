f44590323115553c4068c26b4613cd00a7c7c887
dbrewertx <50505000+dbrewertx@users.noreply.github.com>
2023-07-11 20:45:00 -0500

    added react-bootstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 export default function HomeHeader() {
   return (
     <>
       <h1>Novena</h1>
       <p>A Saint Supplement</p>
-      <Link to="/praynow">Pray Now</Link>
+      <Button variant="primary">
+        <Link to="/praynow">Pray Now</Link>
+      </Button>
     </>
   )
 }
