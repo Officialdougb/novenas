53bbc9c2e0ad834a0af8003b5a0f8a54a0de4a59
dbrewertx <50505000+dbrewertx@users.noreply.github.com>
2023-07-12 20:33:39 -0500

    added home page text and edited pray now button.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,15 @@ import Button from 'react-bootstrap/Button';
 
 export default function HomeHeader() {
   return (
-    <div className="text-center">
+    <div className="text-center ">
+      <p className="mt-5">
+        This app is a helper for users of The Saintmaker planner, providing the full text of novenas suggested in the seasonal prayer guide. 
+      </p>
+      <p className="mt-5 mb-5">
+        For more information about The Saintmaker, visit <a href="https://www.thesaintmaker.com/"> www.thesaintmaker.com</a>
+      </p>
       <Link to="/praynow">
-        <Button variant="primary">
+        <Button className="btn-lg" variant="primary">
           Pray Now
         </Button>
       </Link>
