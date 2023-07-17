53b184fd4d5b48694c08926d63437f6528d19e38
Officialdougb <138799621+Officialdougb@users.noreply.github.com>
2023-07-17 13:23:52 -0500

    added feast/event titles to all novenas

--- src/components/data/SummerNovenas23.js
+++ src/components/data/SummerNovenas23.js
@@ -3,77 +3,77 @@ export const novenas = [
     "title": "Noven to the Precious Blood",
     "text": "By the Voice of Thy Blood, O Jesus! I would press Thee, solicit Thee, importune Thee, though Thou seemest to reject my supplications. I will not leave Thy bleeding feet until Thou hearest me. Too many graces, too many mercies have come from Thy Blood, for me not to hope, even to the end in its efficacy. Then, O Jesus! by the Precious Blood seven times shed for the welfare of men, by each drop of that Sacred Price of our redemption, by the tears of Thy Immaculate Mother, I conjure Thee, I supplicate Thee, hear my earnest prayer. (Mention your intentions here) O Thou, who during all the days of Thy mortal life, consoled so many sufferings, healed so many infirmities, raised so often a sinking courage; Thou will not fail to have pity on a soul who cries to Thee from the depths of her anguish! Oh, no, it is impossible! Another sigh from my heart, and from the wound in Thine own, there will flow to me, upon a wave of Thy merciful Blood, the grace so ardently desired. O Jesus, Jesus! hasten the moment when Thou wilt change my tears into joy, my sighs into thanksgivings. Holy Mary, Source of the Divine Blood! I conjure Thee not to lose this occasion of glorifying the Blood which made Thee Immaculate. Amen.",
     "start": "June 21",
-    "event": "Moth of the Most Precious Blood",
+    "event": "Month of the Most Precious Blood",
     "eventDate": "July 1"
   },
   {
     "title": "Novena to St. Anne",
     "text": "Glorious St. Anne, filled with compassion for those who invoke you and with love for those who suffer, heavily laden with the weight of my troubles, I kneel at your feet and humbly beg you to take my present need under your special protection.(State your intention here)Please recommend it to your daughter, the Blessed Virgin Mary, and lay it before the throne of Jesus. Cease not to intercede for me until my request is granted. Above all, obtain for me the grace to one day meet God face to face, and with you and Mary, and all the angels and saints, to praise Him through all eternity.Great Saint Anne, engrave indelibly on my heart and in my mind the words that have reclaimed and sanctified so many sinners: “What shall it profit a man to gain the whole world if he lose his own soul?”May this be the principal fruit of these prayers by which I will strive to honor you during this novena.At your feet, renew my resolution to invoke you daily, not only for the success of my temporal affairs and to be preserved from sickness and suffering, but above all, that I may be preserved from all sin, that I may gain eternal salvation and that I will receive the special grace of… (state your intention here)O most powerful Saint Anne, do not let me lose my soul, but obtain for me the grace of heaven, there with you, your blessed spouse, and your glorious daughter, to sing the praise of the Most Holy and Adorable Trinity forever and ever. Amen.",
     "start": "July 17",
-    "event": "",
+    "event": "Feast of St. Anne",
     "eventDate": 0
   },
   {
     "title": "Novena to St. Martha",
     "text": "O admirable Saint Martha, I have recourse to thee and I depend entirely on thy intercession in my trials. In thanksgiving, I promise to spread this devotion everywhere. I humbly beg thee to console me in all my difficulties. By the immense joy that filled thy soul when thou didst receive the Redeemer of the world at thy home in Bethany, be pleased to intercede for me and my family, in order that we may keep God in our hearts and therefore, deserve to obtain the remedy to our necessities, especially the present situation that overwhelms me. (mention request) I implore thee, O Auxillatrice in all needs, help us to overcome our difficulties, thou who so victoriously fought the devil. Amen",
     "start": "July 20",
-    "event": "",
+    "event": "Feast of St. Martha",
     "eventDate": 0
   },
   {
     "title": "Novena to St. Dominic",
     "text": "O renowned champion of the faith of Christ, most holy saint Dominic, who didst renounce the honour and dignity of an earthly principality to embrace the poor, laborious, and mortified life which should distinguish a disciple of Him Who has said: "If any man will come after Me, let him take up his cross and follow Me." O burning torch, who being thyself consumed with the fire of divine love, didst incessantly labour to enkindle that sacred flame in the hearts of others, look down upon me from that throne of glory where thou enjoyest the reward of all thy labours, and obtain that some sparks of that blessed fire may be lighted in my soul, to animate and encourage me under any crosses or trials with which it shall please the divine goodness to visit me. Thou, O great Saint, regardest as nothing all the afflictions thou didst endure, and all the toils thou didst undergo for the promotion of God's holy cause; obtain, I beseech Thee, that the same ardent love which strengthened thee, may make sweet to me labours, humiliations, disgraces, or whatever other mortifications I may have to suffer for the Name of Jesus. May I ever bear in mind that nothing can happen to me but by the particular dispensation of a God Who is infinitely wise, and therefore knows what is best for my welfare; infinitely powerful, and consequently able to effect it; and above all, infinitely merciful and loving, Who has laid down His life for my redemption, and continues daily to give new proofs of His love and bounty. O tender Father of the poor who, when all other resources were exhausted, didst offer thyself for their relief, obtain for me that true spirit of compassion for the suffering members of Jesus Christ, which shone forth so conspicuously in thy holy life. May I, by charity to those whom Jesus so dearly loves, lay up for myself treasures in heaven, where thou now enjoyest that which the eye hath not seen, nor the ear heard, nor the heart of man conceived, but which God has prepared for those who love Him, and who prove themselves His Disciples by the observance of His Divine precept "love one another." I praise and thank God for the high degree of sanctity to which He had raised thee, and the special privileges by which He has distinguished thee. I conjure thee, by that gratitude with which thou shalt for all eternity be penetrated for thy Divine Benefactor, implore for me the grace to root out of my heart whatever is not agreeable in His sight, especially that evil habit by which I most frequently offend Him. Obtain likewise the favours I request in this Novena, through thy powerful intercession. (Mention your intentions here) O glorious Mother of God, Queen of the most sacred Rosary, thou who didst love Dominic with the affection of a mother, and were most tenderly loved and honoured by him, look upon me, for his sake, with an eye of pity, deign to join with him in presenting these petitions to thy most Blessed Jesus. I sincerely desire from this moment to love Him with all my heart, and serve Him with all my strength, and now place myself under thy powerful protection, as a sure means of obtaining all the graces necessary to serve Him faithfully here, that I may eternally rejoice with Him hereafter. Amen.",
     "start": "July 26 (1962), July 30 (N.O.)",
-    "event": "",
+    "event": "Feast of St. Dominic",
     "eventDate": 0
   },
   {
     "title": "Novena to St. Clare of Assisi",
     "text": "O most Holy Trinity, Father, Son and Holy Spirit, we praise your Holy Name and the wonders of grace you worked in your servant, Saint Clare. Through her powerful intercession grant us the favours we beg in this novena, above all the grace to live and die as she did in your most Holy Love. Amen.",
     "start": "August 3",
-    "event": "",
+    "event": "Feast of St. Clare",
     "eventDate": 0
   },
   {
-    "title": "Novena to St. Maximilian Kolbe",
+    "title": "Novena to St. Maximillian Kolbe",
     "text": "O Lord Jesus Christ, who said, greater love than this no man has that a man lay down his life for his friends, through the intercession of St. Maximilian Kolbe whose life illustrated such love, we beseech you to grant us our petitions: (mention your requests here) For the Grace to Love Our Lady and Fellow Man as Generously as St. Kolbe Did: Through the Militia Immaculata movement, which Maximilian founded, he spread a fervent devotion to Our Lady throughout the world. He gave up his life for a total stranger and loved his persecutors, giving us an example of unselfish love for all men, a love that was inspired by true devotion to Mary. Grant, O Lord Jesus, that we too may give ourselves entirely without reserve to the love and service of our Heavenly Queen in order to better love and serve our fellowman in imitation of your humble servant, Maximilian. Amen.",
     "start": "August 5",
-    "event": "",
+    "event": "Feast of St. Maximillian",
     "eventDate": 0
   },
   {
     "title": "Novena to St. Philomena",
     "text": "O Faithful Virgin and glorious martyr, St. Philomena, who works so many miracles on behalf of the poor and sorrowing, have pity on me. Thou knowest the multitude and diversity of my needs. Behold me at thy feet, full of misery, but full of hope. I entreat thy charity, O great Saint! Graciously hear me and obtain from God a favorable answer to the request which I now humbly lay before thee.. (mention your intention). I am firmly convinced that through thy merits, through the scorn, the sufferings and the death thou didst endure, united to the merits of the Passion and death of Jesus, thy Spouse, I shall obtain what I ask of thee, and in the joy of my heart I will bless God, who is admirable in His Saints. Amen.",
     "start": "August 31",
-    "event": "",
+    "event": "Feast of St. Philomena",
     "eventDate": 0    
   },
   {
     "title": "Novena to St. Michael the Archangel",
     "text": "Saint Michael the Archangel, loyal champion of God and His Catholic people, I turn to thee with confidence and seek thy powerful intercession. For the love of God, Who hast made thee so glorious in grace and power, and for the love of the Mother of Jesus, the Queen of the Angels, be pleased to hear my prayer. Thou knowest the value of my soul in the eyes of God. May no stain of evil ever disfigure its beauty. Help me to conquer the evil spirit who tempts me. I desire to imitate thy loyalty to God and Holy Mother Church and thy great love for God and men. And since thou art God’s messenger for the care of His people, I entrust to thee this special request: (Here mention your intention or prayer request.) Saint Michael, since thou art, by the will of the Creator, the powerful intercessor of Christians, I have great confidence in thy prayers. I earnestly trust that if it is God’s holy will, my petition will be granted. Pray for me, Saint Michael, and also for those I love. Protect us in all dangers of body and soul. Help us in our daily needs. Through thy powerful intercession, may we live a holy life, die a happy death and reach Heaven where we may praise and love God with thee forever. Amen.",
     "start": "September 20 (1962)",
-    "event": "",    
+    "event": "Dedication of St. Michael the Archangel",    
     "eventDate": 0
   },
   {
     "title": "Novena to Guardian Angel",
     "text": "O holy angel, whom God, by the effect of His goodness and His tender regard for my welfare, has charged with the care of my conduct, and who assists me in all my wants and comforts me in all my afflictions, who supports me when I am discouraged and continually obtains for me new favors. I return thee profound thanks, and I earnestly beseech thee, O most amiable protector, to continue thy charitable care and defense of me against the malignant attacks of all my enemies. I pray especially that you keep me away from all occasions of sin. Obtain for me the grace of listening attentively to thy holy inspirations and of faithfully putting them into practice. In particular, I implore thee to obtain for me the favor which I ask for by this novena (mention your request here…) Protect me in all the temptations and trials of this life, but more especially at the hour of my death, and do not leave me until thou hast conducted me into the presence of God in the mansions of everlasting happiness. Amen.",
     "start": "September 23",
-    "event": "",
+    "event": "Feast of the Holy Guardian Angels",
     "eventDate": 0
   },
   {
     "title": "Novena to St. Therese of Lisieux, the Little Flower",
     "text": "St. Theresa of the Child Jesus, who would have preferred to die rather than lose God's grace, vouchsafe to obtain for us all the help necessary to avoid the misfortune of committing mortal sin, incompatible with this divine grace. Obtain for us the favors we crave and implore through you powerful intercession. Amen.",
     "start": "September 24 (1962), September 22 (N.O.)",
-    "event": "",
+    "event": "Feast of St. Therese",
     "eventDate": 0
   },
   {
     "title": "Novena to St. Francis of Assisi",
     "text": "Glorious Saint Francis, who voluntarily renounced all the comforts and riches of thy home to follow more perfectly the life of poverty and abnegation of Jesus Christ: Obtain for us, we pray, a generous contempt of all things in this world, that we may secure the true and eternal things of heaven. Glory be to the Father, and to the Son, and to the Holy Spirit. Amen. O glorious Saint Francis, who during the whole course of thy life continually wept over the passion of the Redeemer, and labored most zealously for the salvation of souls: Obtain for us, we pray, the grace of weeping continually over those sins by which we have crucified afresh Our Lord Jesus Christ, that we may attain to be of the number of those who shall eternally bless His supreme mercy. Glory be to the Father, and to the Son, and to the Holy Spirit. Amen. O glorious Saint Francis, who, loving above all things suffering and the Cross, merited to bear in thy body the miraculous stigmata, by which thou becamest a living image of Jesus Christ crucified: Obtain for us, we pray, the grace to bear in our bodies the mortifications of Christ, that we may merit one day to receive the consolations which are infallibly promised to all those who now weep. Glory be to the Father, and to the Son, and to the Holy Spirit. Amen.",
     "start": "September 25",
-    "event": "", 
+    "event": "Feast of St. Francis", 
     "eventDate": 0
   },
 ]
