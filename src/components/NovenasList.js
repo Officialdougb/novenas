81f6182b3c3c00d961ffa0fc854255f82988dd5c
Officialdougb <138799621+Officialdougb@users.noreply.github.com>
2023-07-17 13:38:19 -0500

    styled novenas list and text

--- src/components/NovenasList.js
+++ src/components/NovenasList.js
@@ -3,22 +3,16 @@ import Accordion from "react-bootstrap/Accordion";
 export default function NovenasList(props) {
   return (
     <>
-      <style type="text/css">
-        {`
-          ul,li {list-style:none;}
-        `}
-      </style>
-
       {props.data.map((novena) =>
         <Accordion.Item eventKey={novena.title}>
           <Accordion.Header>
             {novena.title}
           </Accordion.Header>
           <Accordion.Body>
-            Start Date: {novena.start}<br />
-            {novena.text}<br />
-            Feast/Event: {novena.event}<br />
-            Feast/Event Date: {novena.eventDate}
+            <div className="fw-bold mb-3">Start Date: {novena.start}</div>
+            <div className="fst-italic mb-3">{novena.text}</div>
+            <div className="fw-bold mb-2">Feast/Event: {novena.event}</div>
+            <div className="fw-bold">Feast/Event Date: {novena.eventDate}</div>
           </Accordion.Body>
         </Accordion.Item>
       )}
